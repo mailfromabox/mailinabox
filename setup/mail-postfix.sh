9cc7b6cacf505a4aab316fbd6d4a565bef41a49a
mailfromabox <33500136+mailfromabox@users.noreply.github.com>
2018-01-22 23:52:16 +0100

    Update mail-postfix.sh
    60s

--- setup/mail-postfix.sh
+++ setup/mail-postfix.sh
@@ -219,7 +219,7 @@ tools/editconf.py /etc/postfix/main.cf \
 
  tools/editconf.py /etc/postfix/main.cf \
  		smtp_destination_concurrency_limit=2 \
-		smtp_destination_rate_delay=160s \
+		smtp_destination_rate_delay=60s \
 		smtp_extra_recipient_limit=1 \
 		qmgr_message_active_limit=200000 \
         qmgr_message_recipient_limit=2000000
